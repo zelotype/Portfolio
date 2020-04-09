2c2a2f64f0276844c570ea410c4103e6a82f85f8
Kawisara Bunyen <zelotype.3@gmail.com>
2020-04-09 17:22:34 +0700

    Updates

---
+++ precache-manifest.0f45cd697dc75fdbed7d84cfee5903e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315e888c6878d1a9f0bde723fd4d3fe7",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "92e452bf91fb6388ee3f",
+    "url": "/Portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/Portfolio/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e452bf91fb6388ee3f",
+    "url": "/Portfolio/static/js/main.dbbc8aa8.chunk.js"
+  },
+  {
+    "revision": "107ce3c625a9fb1de279",
+    "url": "/Portfolio/static/js/runtime-main.e029fae7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87df9f17102ee3e0c849996ee0e4d43a.js"
+  "/Portfolio/precache-manifest.0f45cd697dc75fdbed7d84cfee5903e1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
